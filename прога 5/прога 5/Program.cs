330339333cfbf042ffa6d936ff699cbb743e589a
alinlisov <lisovalina09@gmail.com>
2025-04-16 22:18:36 +0300

    Create Program.cs

---
+++ прога 5/прога 5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// Інтерфейси
+interface IAlphabetDetectable
+{
+    string DetectAlphabet();
+}
+
+interface IOriginDetectable
+{
+    string GetOrigin();
+}
+
+// Абстрактний клас
+abstract class Text
+{
+    protected readonly string content;
+
+    public Text(string content)
+    {
+        this.content = content;
+    }
+
+    public abstract int GetCharacterCount();
+    public abstract void DisplayInfo();
+}
+
+// Клас "Речення"
+class Sentence : Text, IAlphabetDetectable
+{
+    public Sentence(string content) : base(content) { }
+
+    public override int GetCharacterCount() => content.Length;
+
+    
